12902444e7f95e9df65b246990bc501723b5852f
firmanserdana <fthecatrock@gmail.com>
2023-11-15 16:10:15 +0100

    some progress untested

--- Regrasp_2nd/main.py
+++ Regrasp_2nd/main.py
@@ -1,16 +1,26 @@
-# FILEPATH: /home/firep1/Documents/gitworks/phd/ReWire/Rewire_Project/Regrasp_2nd/main.py
-
 import matlab.engine
-#import communication
+import numpy as np
+import read_inputs as inputs
+
+ip_address = '0.0.0.0'
+port = 45454
 
 # Connect to Grapevine
 eng = matlab.engine.start_matlab()
-eng.cd(r'Regrasp_2nd')
-eng.stimseq_matlab(nargout=0)
-# Connect to Sessantaquattro+
+# eng.cd(r'Regrasp_2nd')
 
-# Set up input channel for Sessantaquattro+
+# Connect to Sessantaquattro+ and read data
+# This is a placeholder - replace with actual code to read data from Sessantaquattro+
+emg_data = inputs.read_inputs(ip_address, port)  # example data
 
-# Set up output channel for Grapevine's stimulator
+# Extract the necessary variables from the EMG data
+env = emg_data  # replace with the actual extraction logic
+env_thr = np.mean(emg_data)  # replace with the actual extraction logic
+forceRange_mV = np.max(emg_data) - np.min(emg_data)  # replace with the actual extraction logic
+cs = np.argmax(emg_data)  # replace with the actual extraction logic
+stimChans = [0, 1, 2]  # replace with the actual extraction logic
+
+# Call stimseq_matlab with extracted variables
+eng.controlStimulation(env, env_thr, forceRange_mV, cs, stimChans, nargout=0)
 
 # Start streaming data from Sessantaquattro+ and sending it to Grapevine's stimulator
--- Regrasp_2nd/read_inputs.py
+++ Regrasp_2nd/read_inputs.py
@@ -1,24 +1,58 @@
-import xipppy as xp
+import socket
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import communication
+import xsensdeviceapi as xsens
 
-# Open a connection to the device
-xp.open()
+# ... (rest of your imports and class definitions)
 
-# Read some data
-data = xp.read()
+def read_inputs(ip_address, port):
+    # Create a socket which is used to connect to Sessantaquattro
+    sq_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sq_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sq_socket.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
 
-# Close the connection when done
-xp.close()
+    # Create start command and get basic setup information
+    (start_command,
+    number_of_channels,
+    sample_frequency,
+    bytes_in_sample) = communication.create_bin_command(start=1)
 
-from xsensdeviceapi import XsensDeviceApi
+    sample_from_channels = [0 for i in range(number_of_channels - 8)]
 
-# Create a new API instance
-api = XsensDeviceApi()
+    print('Starting to log data: {0} channels with {1} sampling rate'.format(number_of_channels, sample_frequency))
+    print("number of channels " + str(number_of_channels))
+    # Open connection to Sessantaquattro
+    connection = communication.connect_to_sq(sq_socket, ip_address, port, start_command)
 
-# Connect to the device
-api.connect_device()
+    last_time = {0: time.time()}
 
-# Read some data
-data = api.get_data()
+    lines = []
+    line = []
 
-# Disconnect when done
-api.disconnect_device()
+    s = SoundTrack(number_of_channels - 8)
+    i = 0
+
+    while True:
+        sample_from_channels_as_bytes = communication.read_raw_bytes(
+            connection,
+            number_of_channels,
+            bytes_in_sample)
+
+        # Convert the bytes into integer values
+        sample_from_channels = communication.bytes_to_integers(
+            sample_from_channels_as_bytes,
+            number_of_channels,
+            bytes_in_sample,
+            output_milli_volts=False)
+        i += 1
+
+        if i % (sample_frequency / 10) == 0:
+            new_time = time.time()
+            s.update(sample_from_channels)
+            print("{0:.2f} fps".format(1. / (new_time - last_time[0])))
+            last_time.update({0: new_time})
+            i = 0
+
+    tracks = s.get_tracks()
